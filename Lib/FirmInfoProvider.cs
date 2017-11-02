1966b0a28c6ecf0b88247efeabcf517ee3884228
Stanislav Gulis <stanislav.gulis@gmail.com>
2017-11-02 09:25:28 +0100

    Prebytočný interface...

--- Lib/FirmInfoProvider.cs
+++ Lib/FirmInfoProvider.cs
@@ -4,17 +4,6 @@ using System.Collections.Generic;
 namespace EnumerableDemo
 {
 
-    public interface IFirmInfoProvider
-    {
-
-        /// <summary>
-        /// Vráti zoznam podrobných informácií o pripojených firmách.
-        /// Pre vytiahnutie týchto info je potrebné firmu otvoriť, potom vyvolať Dispose.
-        /// </summary>
-        IEnumerable<FirmInfo> GetFirmInfos();
-
-    }
-
     public class FirmInfoProvider
     {
 
